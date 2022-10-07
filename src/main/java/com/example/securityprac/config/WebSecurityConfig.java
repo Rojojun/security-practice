6ddba38f81a31bab0cdfb5b2861598b03b8fda28
rojojun <zoloman316@gmail.com>
2022-10-07 19:15:32 +0900

    [feature] Spring Security FilterChain에 등록

--- src/main/java/com/example/securityprac/config/WebSecurityConfig.java
+++ src/main/java/com/example/securityprac/config/WebSecurityConfig.java
@@ -1,5 +1,25 @@
 package com.example.securityprac.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
+// Spring Security Filter를 Spring FilterChain에 등록
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+    }
 }
