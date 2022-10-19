630a15268b6b66d281a713e5db6e8a884dd6bc96
rojojun <zoloman316@gmail.com>
2022-10-19 10:17:10 +0900

    [feat] SpringBoot, OAuth2.0, Seucrity 구현 완료

--- src/main/java/com/example/securityprac/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/securityprac/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.example.securityprac.config.oauth;
 import com.example.securityprac.config.auth.PrincipalDetails;
 import com.example.securityprac.config.oauth.provider.FacebookUserInfo;
 import com.example.securityprac.config.oauth.provider.GoogleUserInfo;
+import com.example.securityprac.config.oauth.provider.NaverUserInfo;
 import com.example.securityprac.config.oauth.provider.OAuth2UserInfo;
 import com.example.securityprac.model.Member;
 import com.example.securityprac.repository.UserRepository;
@@ -13,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -43,6 +46,9 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("FaceBook");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("Naver");
+            oAuth2UserInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
         } else {
             throw new RuntimeException("지원하지 않는 OAuth");
         }
